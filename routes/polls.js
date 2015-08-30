9e8eae9323774eb478688c527fff4033b6213365
Edgar Cabrera <edgar@cafeinacode.com>
2015-08-30 18:10:49 -0500

    Add basic methods for interacting with polls.
    Create basic entry points for creating, searching and
    deleting existing polls, together with their tests.

--- routes/polls.js
+++ routes/polls.js
@@ -3,22 +3,53 @@
 var express = require('express');
 var router = express.Router();
 var Poll = require('../models/poll');
+var bodyParserJSON = require('body-parser').json();
 
 router.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.send('ok');
   })
-  .post(function (req, res) {
-    res.status(201);
-    res.send('ok');
+  .post(bodyParserJSON, function(req, res) {
+    var p = new Poll(req.body);
+    p.save(function(err, poll) {
+      if (err) {
+        res.estatus(400);
+        res.send(err);
+      } else {
+        res.status(201);
+        res.send(poll);
+      }
+    });
   });
 
 router.route('/:id')
-  .get(function (req, res) {
-    res.send('ok');
+  .get(function(req, res) {
+    var id = req.params.id;
+    Poll.findOne({_id: id})
+      .then(function(poll) {
+        res.send(poll);
+      },
+      function(err) {
+        res.status(404);
+        res.send({error: "Poll '" + id + "' not found"});
+      });
   })
-  .delete(function (req, res) {
-    res.send('ok');
+  .delete(function(req, res) {
+    var id = req.params.id;
+    Poll.findOne({_id: id})
+      .then(function(poll) {
+        poll.remove(function(err) {
+          if (err) {
+            res.status(500);
+            res.status({error: "Could not delete poll '" + id + "'"});
+          } else {
+            res.send(poll);
+          }
+        });
+      }, function() {
+        res.status(404);
+        res.send({error: "Poll '" + id + "' not found"});
+      });
   });
 
 module.exports = router;
--- test/test_poll_handler.js
+++ test/test_poll_handler.js
@@ -1,9 +1,16 @@
 'use strict';
 
 var request = require('supertest');
+var should = require('should');
+var mongoose = require('mongoose');
 var app = require('../app');
+var Poll = require('../models/poll');
 
 describe('poll route handler', function() {
+  before(function (done) {
+    mongoose.connection.db.dropDatabase(done);
+  });
+
   it('responds successfuly to the listing route', function(done) {
     request(app)
       .get('/poll')
@@ -11,15 +18,80 @@ describe('poll route handler', function() {
   });
 
   it('can create a poll', function(done) {
-    var params = {};
+    var params = {
+      questions: [1,2,3]
+    };
     request(app)
       .post('/poll')
       .send(params)
-      .expect(201, done);
+      .expect(201)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.have.property('_id');
+        res.body.should.have.property('questions', params.questions);
+
+        Poll.findOne({_id: res.body._id}, function(err, p) {
+          if (err) return done(err);
+          p.should.be.an.Object();
+          p.questions.should.be.an.Array();
+          done();
+        });
+      });
   });
 
   it('can return an existing poll', function(done) {
-    //TODO
-    done();
+    var p = new Poll({questions: [1, 2, 3]});
+    p.save(function(err, poll) {
+      var id = poll._id.toString();
+      request(app)
+        .get('/poll/' + id)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.should.have.property('_id', id);
+          res.body.should.have.property('questions', [1, 2, 3]);
+          done();
+        });
+    });
+  });
+
+  it('indicates a non existing poll', function(done) {
+    request(app)
+      .get('/poll/nonexistingobviously')
+      .expect(404, done);
+  });
+
+  it('can destroy a poll', function(done) {
+    var p = new Poll({questions: [1, 2, 3]});
+
+    function shouldNotExist(id) {
+      Poll.findOne({_id: id})
+        .then(function(poll) {
+          if (poll) {
+            done(new Error("Object still exists"));
+          } else {
+            done();
+          }
+        });
+    }
+
+    p.save(function(err, poll) {
+      var id = poll._id.toString();
+      request(app)
+        .del('/poll/' + id)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.should.have.property('_id', id);
+          res.body.should.have.property('questions', [1, 2, 3]);
+          shouldNotExist(id);
+        });
+    });
+  });
+
+  it('cannot delete a non existing poll', function(done) {
+    request(app)
+      .del('/poll/nonexistingobviously')
+      .expect(404, done);
   });
 });
