1af8461246d3b41ab47c51606ef2713dca8bf031
lgovea <luisgovea.40@gmail.com>
2015-09-01 22:49:25 -0500

    Add field options to question model

--- models/question.js
+++ models/question.js
@@ -4,9 +4,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var questionSchema = new Schema({
-  question : String,
+  question : { type: String, required: true, unique:true },
   answerDescription : {
-    answerDataType: String,
+    answerDataType: { type:String, required:true },
     answerComment: String,
     answerOptions: Schema.Types.Mixed,
     relatedQuestion: Schema.Types.Mixed
